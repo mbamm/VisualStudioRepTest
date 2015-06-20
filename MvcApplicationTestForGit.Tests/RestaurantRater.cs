3e3b9f8cb4b727589c0868d82b4ea590edecf192
Mike B <mikebamm@gmail.com>
2015-06-19 20:54:49 -0700

    commited change

---
+++ MvcApplicationTestForGit.Tests/RestaurantRater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcApplicationTestForGit.Tests
+{
+    class RestaurantRater
+    {
+        public int ComputeRating()
+        {
+            return 3;
+            //commit#1
+        }
+    }
+}
